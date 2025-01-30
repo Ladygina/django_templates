fd37be81ce0aed070d577e6dce14fa0f1685312f
Ladygina <101057953+Ladygina@users.noreply.github.com>
2025-01-30 17:03:32 +0300

    Update views.py

--- calculator/views.py
+++ calculator/views.py
@@ -20,27 +20,6 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-context = {
-  'omlet': {
-    'яйца, шт': 2,
-    'молоко, л': 0.1,
-    'соль, ч.л.': 0.5,
-  },
-  'pasta': {
-    'макароны, г': 0.3,
-     'сыр, г': 0.05,
-    },
-    'buter': {
-            'хлеб, ломтик': 1,
-            'колбаса, ломтик': 1,
-            'сыр, ломтик': 1,
-            'помидор, ломтик': 1,
-    }
-}
 recipe_omlet = {
         'яйца, шт': 2,
         'молоко, л': 0.1,
@@ -58,9 +37,6 @@ recipe_buter = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
 }
-
-# def index(request):
-#     return render(request, "calculator/index.html", context= context)
 
 def index_omlet(request):
     count_recipe = request.GET.get('serving')
